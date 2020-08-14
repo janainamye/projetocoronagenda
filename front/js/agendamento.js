cc766db1d94f21b90ac16dd31dd9244d228e3541
janainamye <janainamye@yahoo.com.br>
2020-08-14 14:06:21 -0300

    projeto4

--- front/js/agendamento.js
+++ front/js/agendamento.js
@@ -5,6 +5,8 @@ function cadastrar(event) {
     let email=document.getElementById("txtEmail");
     let cellWpp=document.getElementById("txtCellWpp");
     let data=document.getElementById("txtData");
+    ("#data").datepicker({ minDate: 0});
+
     let agencia=document.getElementById("txtAgencia");
     let horaInicio=document.getElementById("txtHoraInicio");
     //let horaFim=document.getElementById("txtHoraFim");
@@ -18,8 +20,9 @@ function cadastrar(event) {
         horaAgendamento: horaInicio.value,
         //observacao: "null",
         agencia: {id: agencia.value}
+
     }
- 
+
     let cabecalho={
         method: 'POST',
         body: JSON.stringify(cadastrarMsg),
@@ -27,8 +30,31 @@ function cadastrar(event) {
             'Content-type':'application/json' //padrão qndo é json
         }
     }
- 
     //envia o pedido para o servidor 
     fetch('http://localhost:8080/agendamento/novo', cabecalho);
  
+}
+
+function obterAgencias() {
+    fetch("http://localhost:8080/agencias")
+        .then(res => res.json())
+        .then(res => preencheAgencias(res));
+}
+
+function preencheAgencias(res) {
+    let agencias = '';
+
+    agencias = `<option hidden > Escolha uma opção</option>`;
+    for (let index = 0; index < res.length; index++) {
+        agencias = agencias + `<option value = ${res[index].id}> ${res[index].nome} </option>`;
+    }
+    document.getElementById("sel_agencias").innerHTML = agencias;
+    
+    /*
+    let hoje = Date.now();
+    hoje = hoje.format("d-m-Y");
+    document.getElementById("campodata").innerHTML = `<input class="form-control" type="date" id="txtData" placeholder="Data" min="${hoje}"> `
+    */
+
+
 }
